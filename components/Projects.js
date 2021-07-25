490e5188e63ee5687931f3a05f685ae0741481de
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-07-25 15:55:56 -0700

    Delete right-side blank on Projects.js

--- components/Projects.js
+++ components/Projects.js
@@ -73,7 +73,7 @@ const Projects = () => {
 
   return (
     <Container className={classes.content} id="projects">
-      <Typography align="center" gutterBottom="true" variant="h2" className={classes.title}>PROJECTS</Typography>
+      <Typography align="center" gutterBottom="true" variant="h3" className={classes.title}>PROJECTS</Typography>
       <Typography align="center" gutterBottom="true" variants="h2" style={{fontSize:"45px", textDecoration: "underline"}}>Web Projects</Typography>
       {/* Web project area */}
       <Grid container spacing={16}  justify="space-evenly">
