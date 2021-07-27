9d10eb8b6d7902e633348e47c02a16db42263e9b
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-07-27 09:00:18 -0700

    Change title on Main.js

--- components/Main.js
+++ components/Main.js
@@ -14,17 +14,6 @@ const useStyles = makeStyles({
     padding: 0,
     margin: 0
   },
-  position: {
-    fontFamily: "Helvetice Neue Droid Serif  Helvetica Arial sans-serif",
-    fontWeight: 'bolder',
-    fontSize: "40px",
-    fontStyle: "italic",
-  },
-  name: {
-    textTransform: "uppercase",
-    fontWeight: "700",
-    margin: "25px"
-  },
   contactIcon: {
     marginTop: "100px",
     width: "100%"
@@ -43,30 +32,31 @@ const useStyles = makeStyles({
 const Main = () => {
   const classes = useStyles();
   return (
-    <Container className={classes.mainContent} id="main">
-      <Typography className={classes.position} align="center">Web Software Developer</Typography>
-      <Typography className={classes.name} align="center" variant="h3">Kazunobu Someya</Typography>
-      <Grid container justify="center" alignItems="center" direction="row" className={classes.contactIcon}>
-        <IconButton href="https://github.com/wangkalbi-gernessen" target="_blank">
-          <GitHubIcon className={classes.images} fontSize="large" />
-        </IconButton>
-        <IconButton href="https://www.linkedin.com/in/kazunobu-someya-6b44901b6/" target="_blank">
-          <LinkedInIcon className={classes.images} />
-        </IconButton>
-        <IconButton href={`mailto: guizhou08chishui@gmail.com`} target="_blank" >
-          <EmailIcon className={classes.images} />
-        </IconButton>
-        <IconButton href={`tel: 778-689-7760`} target="_blank" >
-          <CallIcon className={classes.images} />
-        </IconButton>
-        <IconButton href="https://www.facebook.com/kazunobu.someya" target="_blank" >
-          <FacebookIcon className={classes.images} />
-        </IconButton>
-        <IconButton href="https://www.instagram.com/kazukeks/" target="_blank">
-          <InstagramIcon className={classes.images} />
-        </IconButton>
-      </Grid>
-    </Container>   
+    <Grid container spacing={0}direction="row" alignItems="center" justify="center" className={classes.mainContent} id="main">
+      <Grid item xs={11}>
+        <Typography align="center" variant="h4">Hello, I'm <span style={{color: "red"}}>Kazunobu Someya</span>.<br />I'm a full-stack web developer.</Typography>
+        <Grid container justify="center" alignItems="center" direction="row" className={classes.contactIcon}>
+          <IconButton href="https://github.com/wangkalbi-gernessen" target="_blank">
+            <GitHubIcon className={classes.images} fontSize="large" />
+          </IconButton>
+          <IconButton href="https://www.linkedin.com/in/kazunobu-someya-6b44901b6/" target="_blank">
+            <LinkedInIcon className={classes.images} />
+          </IconButton>
+          <IconButton href={`mailto: guizhou08chishui@gmail.com`} target="_blank" >
+            <EmailIcon className={classes.images} />
+          </IconButton>
+          <IconButton href={`tel: 778-689-7760`} target="_blank" >
+            <CallIcon className={classes.images} />
+          </IconButton>
+          <IconButton href="https://www.facebook.com/kazunobu.someya" target="_blank" >
+            <FacebookIcon className={classes.images} />
+          </IconButton>
+          <IconButton href="https://www.instagram.com/kazukeks/" target="_blank">
+            <InstagramIcon className={classes.images} />
+          </IconButton>
+        </Grid>
+      </Grid>   
+    </Grid>
   );
 }
 
