4860af38e92541093fb63c5e6e89b70fc5aa954b
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-08-02 03:27:05 -0700

    Change grid breakpoints on Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -12,12 +12,13 @@ const useStyle = makeStyles({
     margin: 0,
     padding: 0,
     minWidth: "100%",
-    minHeight: "50vh",
+    minHeight: "20vh",
     background: "white"
   }, 
   title: {
     fontWeight: 700,
     paddingTop: "40px",
+    paddingBottom: "60px",
     textDecoration: "underline"
   }
 });
@@ -27,33 +28,33 @@ const Contact = () => {
   return(
     <Container className={classes.content} id="contact">
       <Typography variant="h3" align="center" className={classes.title}>Contact</Typography>
-      <Grid container justify="center" spacing={4} className={classes.icons}>
-        <Grid item xs={4}>
+      <Grid container spacing={0}>
+        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
           <IconButton href="https://github.com/wangkalbi-gernessen" target="_blank">
             <GitHubIcon className={classes.images} />
           </IconButton>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
           <IconButton href="https://www.linkedin.com/in/kazunobu-someya-6b44901b6/" target="_blank">
             <LinkedInIcon className={classes.images} />
           </IconButton>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
           <IconButton href={`mailto: guizhou08chishui@gmail.com`} target="_blank" >
             <EmailIcon className={classes.images} />
           </IconButton>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
           <IconButton href={`tel: 778-689-7760`} target="_blank" >
             <CallIcon className={classes.images} />
           </IconButton>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
           <IconButton href="https://www.facebook.com/kazunobu.someya" target="_blank" >
             <FacebookIcon className={classes.images} />
           </IconButton>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
           <IconButton href="https://www.instagram.com/kazukeks/" target="_blank">
             <InstagramIcon className={classes.images} />
           </IconButton>
