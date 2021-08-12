a882dce079d15a4b5d5584f3e0a214e138eec907
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-08-11 18:32:19 -0700

    Delete Facebook and Instagram icon on Contact

--- components/Contact.js
+++ components/Contact.js
@@ -18,8 +18,11 @@ const useStyle = makeStyles({
   title: {
     fontWeight: 700,
     paddingTop: "40px",
-    paddingBottom: "60px",
+    paddingBottom: "30px",
     textDecoration: "underline"
+  },
+  images: {
+    fontSize: "40px"
   }
 });
 
@@ -28,35 +31,27 @@ const Contact = () => {
   return(
     <Container className={classes.content} id="contact">
       <Typography variant="h3" align="center" className={classes.title}>Contact</Typography>
+      <Typography align="center" style={{fontSize: "40px"}}>Thanks for taking your time to reach out.</Typography>
+      <Typography align="center" style={{fontSize: "40px"}}>How can I help you today?</Typography>
       <Grid container spacing={0}>
-        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
+        <Grid item xs={6} sm={3} style={{textAlign: "center"}}>
           <IconButton href="https://github.com/wangkalbi-gernessen" target="_blank">
             <GitHubIcon className={classes.images} />
           </IconButton>
         </Grid>
-        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
+        <Grid item xs={6} sm={3} style={{textAlign: "center"}}>
           <IconButton href="https://www.linkedin.com/in/kazunobu-someya-6b44901b6/" target="_blank">
             <LinkedInIcon className={classes.images} />
           </IconButton>
         </Grid>
-        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
+        <Grid item xs={6} sm={3} style={{textAlign: "center"}}>
           <IconButton href={`mailto: guizhou08chishui@gmail.com`} target="_blank" >
             <EmailIcon className={classes.images} />
           </IconButton>
         </Grid>
-        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
+        <Grid item xs={6} sm={3} style={{textAlign: "center"}}>
           <IconButton href={`tel: 778-689-7760`} target="_blank" >
             <CallIcon className={classes.images} />
-          </IconButton>
-        </Grid>
-        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
-          <IconButton href="https://www.facebook.com/kazunobu.someya" target="_blank" >
-            <FacebookIcon className={classes.images} />
-          </IconButton>
-        </Grid>
-        <Grid item xs={4} sm={2} style={{textAlign: "center"}}>
-          <IconButton href="https://www.instagram.com/kazukeks/" target="_blank">
-            <InstagramIcon className={classes.images} />
           </IconButton>
         </Grid>
       </Grid>
