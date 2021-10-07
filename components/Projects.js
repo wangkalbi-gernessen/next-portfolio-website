23949e240387e4763c4cfa0c3b9252e99dc009e3
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-10-06 23:10:07 -0700

    Comment out useState on Projects

--- components/Projects.js
+++ components/Projects.js
@@ -1,7 +1,7 @@
 import { CardActions, CardContent, CardHeader, CardMedia, Container, makeStyles, Button, Typography } from "@material-ui/core";
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import {useState} from 'react';
+// import {useState} from 'react';
 
 const useStyles = makeStyles({
   content: {
