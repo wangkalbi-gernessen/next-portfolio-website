8cd747074ddfa770d71984869adb9c799d42247d
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2023-01-06 14:21:55 -0800

    Move page components to pages folder

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { Grid, Container, Typography } from '@material-ui/core';
-import Profile from './Profile';
-import Skills from './Skills';
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Grid, Container, Typography } from "@material-ui/core";
+import Profile from "../components/Profile";
+import Skills from "../components/Skills";
 
 const useStyle = makeStyles({
   content: {
@@ -10,7 +10,7 @@ const useStyle = makeStyles({
     padding: "20px",
     minWidth: "100vw",
     minHeight: "80vh",
-    background: "white"
+    background: "white",
   },
   title: {
     fontWeight: "700",
@@ -18,35 +18,37 @@ const useStyle = makeStyles({
     position: "relative",
     zIndex: 1,
     paddingBottom: "5px",
-    '&::after': {
+    "&::after": {
       content: "' '",
       position: "absolute",
       left: "50%",
       marginLeft: "-50px",
       bottom: 0,
       width: "100px",
-      borderBottom: "3px solid magenta"
-    }
-  }
+      borderBottom: "3px solid magenta",
+    },
+  },
 });
 
 const Aboutme = () => {
   const classes = useStyle();
-  return(
+  return (
     <Grid container className={classes.content} id="aboutme">
       <Grid item xs={12} sm={12} md={12} xl={12} lg={12}>
-        <Typography align="center" variant="h3" className={classes.title}>About Me</Typography>
+        <Typography align="center" variant="h3" className={classes.title}>
+          About Me
+        </Typography>
         <Grid container>
           <Grid item xs={12} sm={12} md={6} xl={6} lg={6}>
-            <Profile/>  
+            <Profile />
           </Grid>
           <Grid item xs={12} sm={12} md={6} xl={6} lg={6}>
-            <Skills/>
+            <Skills />
           </Grid>
         </Grid>
       </Grid>
     </Grid>
   );
-}
+};
 
 export default Aboutme;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
-import Navbar from '../components/Navbar';
-import Main from '../components/Main';
-import Aboutme from '../components/Aboutme';
-import Projects from '../components/Projects';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-import { Grid, Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import Navbar from "../components/Navbar";
+import Main from "./main";
+import Aboutme from "./aboutme";
+import Projects from "./projects";
+import Contact from "./contact";
+import Footer from "./footer";
+import { Grid, Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 
 const useStyle = makeStyles({
   content: {
     margin: 0,
     padding: 0,
     minWidth: "100vw",
-    minHeight: "100vh"
-  }
+    minHeight: "100vh",
+  },
 });
 
 export default function Home() {
@@ -27,5 +27,5 @@ export default function Home() {
       <Contact />
       <Footer />
     </Container>
-  )
+  );
 }
